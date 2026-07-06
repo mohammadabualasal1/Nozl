fa1aca6a0b5ea02a398666ce51c3e1f1c72d128c
moham <moham@OMAR_ALKHATEEB>
2026-07-06 18:25:11 +0300

    hotel controller 1\2

---
+++ Controllers/HotelController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Nozl.DTOs.HotelDtos;
+using Nozl.Model;
+using System.Security.Claims;
+
+namespace Nozl.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HotelController : ControllerBase
+    {
+           private NozlDbContext _dbContext;
+
+            public HotelController(NozlDbContext dbContext)
+            {
+                _dbContext = dbContext;
+            }
+
+        [HttpGet("GetAllHotels")]
+        [Authorize]
+        public async Task<IActionResult> GetAllHotels()
+        {
+            var UserIdString = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            var UserId = int.Parse(UserIdString);
+
+            var hotel = _dbContext.Hotels.Select(h => new
+            {
+                h.Name,
+                h.ImageUrl,
+                h.Owner,
+                h.Address,
+                h.City,
+                h.Description,
+                h.Phone
+            });
+          return Ok(hotel);
+        }
+        [HttpGet("GetById")]
+        [Authorize]
+        public async Task<IActionResult> GetById(long id)
+        {
+
+            var UserIdString = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            var UserId = int.Parse(UserIdString);
+
+            var hotel = from h in _dbContext.Hotels
+                        where h.Id == id
+                        select new
+                        {
+                            h.Name,
+                            h.ImageUrl,
+                            h.Owner,
+                            h.Address,
+                            h.City,
+                            h.Description,
+                            h.Phone
+                        };
+            var data = await hotel.ToListAsync();
+            return Ok(hotel);   
+        }
+        [HttpPost("AddNewHotel")]
+        [Authorize (Roles ="HotelOwner")]
+        public async Task<IActionResult> AddNewHotel(AddHotelDto dto)
+        {
+            var UserIdString = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            var UserId = int.Parse(UserIdString);
+
+            var hotel = new Hotel
+            {
+                OwnerId = UserId,
+                Name = dto.Name,
+                ImageUrl = dto.ImageUrl,
+                City = dto.City,
+                Description = dto.Description,
+                Status ="Pending",
+                Phone = dto.Phone,
+                Address = dto.Address,
+
+            };
+            _dbContext.Hotels.Add(hotel);
+            await _dbContext.SaveChangesAsync();
+             return Ok(hotel);
+
+
+        }
+     }
+}
---
+++ DTOs/HotelDtos/AddHotelDto.cs
@@ -0,0 +1,18 @@
+﻿using Nozl.Model;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Nozl.DTOs.HotelDtos
+{
+    public class AddHotelDto
+    {
+      
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public string Status { get; set; }
+
+    }
+}
