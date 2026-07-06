66ddc78f9282991964c0b3d2a3f84e0777ea48cd
moham <moham@OMAR_ALKHATEEB>
2026-07-06 14:55:43 +0300

    edit phone

---
+++ Migrations/20260706115532_m3.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Nozl.Migrations
+{
+    /// <inheritdoc />
+    public partial class m3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Phone",
+                table: "Hotels",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Phone",
+                table: "Hotels");
+        }
+    }
+}
--- Model/Hotel.cs
+++ Model/Hotel.cs
@@ -12,6 +12,7 @@ namespace Nozl.Model
         public string Name { get; set; }
         public string City { get; set; }
         public string Address { get; set; }
+        public string Phone { get; set; }
         public string Description { get; set; }
         public string ImageUrl { get; set; }
         public string Status { get; set; } 
